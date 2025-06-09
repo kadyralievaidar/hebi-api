b66b4feb88466b20e7f7e91749e4d6a38a83f1ff
Kanykei470 <akanykej73@gmail.com>
2025-06-10 01:59:48 +0600

    Created Validator for RegisterUseDto. Added tests.

---
+++ Hebi-Api.Tests/Appoitments/Validators/RegisterUserRequestValidatorTests.cs
@@ -0,0 +1,391 @@
+﻿using Hebi_Api.Features.Users.RequestHandling.Validators;
+using FluentValidation.TestHelper;
+using Hebi_Api.Features.Users.Dtos;
+using NUnit.Framework;
+using Hebi_Api.Features.Users.RequestHandling.Requests;
+
+namespace Hebi_Api.Tests.Appoitments.Validators;
+
+[TestFixture]
+public class RegisterUserRequestValidatorTests
+{
+    private RegisterUserRequestValidator _validator;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _validator = new RegisterUserRequestValidator();
+    }
+
+    [Test]
+    public void Should_Have_Error_When_UserName_Is_Empty()
+    {
+        // Arrange
+        var dto = new RegisterUserDto
+        {
+            UserName = "",
+            Password = "TesT*00",
+            ConfirmPassword = "TesT*00",
+            Email = "Test@mail.com",
+            FirstName = "Test",
+            LastName = "Test",
+            PhoneNumber = "955587878895"
+        };
+
+        // Act
+        var result = _validator.TestValidate(new RegisterUserRequest(dto));
+
+        // Assert
+        result.ShouldHaveValidationErrorFor(x => x.RegisterUserDto.UserName)
+              .WithErrorMessage("UserName is required");
+    }
+
+    [Test]
+    public void Should_Have_Error_When_UserName_Is_Short()
+    {
+        // Arrange
+        var dto = new RegisterUserDto
+        {
+            UserName = "ky",
+            Password = "TesT*00",
+            ConfirmPassword = "TesT*00",
+            Email = "Test@mail.com",
+            FirstName = "Test",
+            LastName = "Test",
+            PhoneNumber = "955587878895"
+        };
+        // Act
+        var result = _validator.TestValidate(new RegisterUserRequest(dto));
+        // Assert
+        result.ShouldHaveValidationErrorFor(x => x.RegisterUserDto.UserName)
+            .WithErrorMessage("UserName must be at least 4 characters");
+    }
+
+    [Test]
+    public void Should_Have_Error_When_Password_Is_Empty()
+    {
+        // Arrange
+        var dto = new RegisterUserDto
+        {
+            UserName = "Test1",
+            Password = "",
+            ConfirmPassword = "",
+            Email = "Test@mail.com",
+            FirstName = "Test",
+            LastName = "Test",
+            PhoneNumber = "955587878895"
+        };
+        // Act
+        var result = _validator.TestValidate(new RegisterUserRequest(dto));
+        // Assert
+        result.ShouldHaveValidationErrorFor(x => x.RegisterUserDto.Password)
+            .WithErrorMessage("Password is required");
+    }
+
+    [Test]
+    public void Should_Have_Error_When_Password_Is_Too_Short()
+    {
+        // Arrange
+        var dto = new RegisterUserDto
+        {
+            UserName = "Test1",
+            Password = "Aa1*",
+            ConfirmPassword = "Aa1*",
+            Email = "Test@mail.com",
+            FirstName = "Test",
+            LastName = "Test",
+            PhoneNumber = "955587878895"
+
+        };
+        // Act
+        var result = _validator.TestValidate(new RegisterUserRequest(dto));
+        // Assert
+        result.ShouldHaveValidationErrorFor(x => x.RegisterUserDto.Password)
+            .WithErrorMessage("Password must be at least 6 characters");
+    }
+
+    [Test]
+    public void Should_Have_Error_When_Password_Has_No_Uppercase()
+    {
+        // Arrange
+        var dto = new RegisterUserDto
+        {
+            UserName = "Test1",
+            Password = "test*123",
+            ConfirmPassword = "test*123",
+            Email = "Test@mail.com",
+            FirstName = "Test",
+            LastName = "Test",
+            PhoneNumber = "955587878895"
+
+        };
+        // Act
+        var result = _validator.TestValidate(new RegisterUserRequest(dto));
+        // Assert
+        result.ShouldHaveValidationErrorFor(x => x.RegisterUserDto.Password)
+            .WithErrorMessage("Password must contain at least one uppercase letter");
+    }
+
+    [Test]
+    public void Should_Have_Error_When_Password_Has_No_Lowercase()
+    {
+        // Arrange
+        var dto = new RegisterUserDto
+        {
+            UserName = "Test1",
+            Password = "TEST*123",
+            ConfirmPassword = "TEST*123",
+            Email = "Test@mail.com",
+            FirstName = "Test",
+            LastName = "Test",
+            PhoneNumber = "955587878895"
+
+        };
+        // Act
+        var result = _validator.TestValidate(new RegisterUserRequest(dto));
+        // Assert
+        result.ShouldHaveValidationErrorFor(x => x.RegisterUserDto.Password)
+            .WithErrorMessage("Password must contain at least one lowercase letter");
+    }
+
+    [Test]
+    public void Should_Have_Error_When_Password_Has_No_Number()
+    {
+        // Arrange
+        var dto = new RegisterUserDto
+        {
+            UserName = "Test1",
+            Password = "Test*Test",
+            ConfirmPassword = "Test*Test",
+            Email = "Test@mail.com",
+            FirstName = "Test",
+            LastName = "Test",
+            PhoneNumber = "955587878895"
+
+        };
+        // Act
+        var result = _validator.TestValidate(new RegisterUserRequest(dto));
+        // Assert
+        result.ShouldHaveValidationErrorFor(x => x.RegisterUserDto.Password)
+            .WithErrorMessage("Password must contain at least one number");
+    }
+
+    [Test]
+    public void Should_Have_Error_When_Password_Has_No_Special_Character()
+    {
+        // Arrange
+        var dto = new RegisterUserDto
+        {
+            UserName = "Test1",
+            Password = "Test1234",
+            ConfirmPassword = "Test1234",
+            Email = "Test@mail.com",
+            FirstName = "Test",
+            LastName = "Test",
+            PhoneNumber = "955587878895"
+
+        };
+        // Act
+        var result = _validator.TestValidate(new RegisterUserRequest(dto));
+        // Assert
+        result.ShouldHaveValidationErrorFor(x => x.RegisterUserDto.Password)
+            .WithErrorMessage("Password must contain at least one special character");
+    }
+
+    [Test]
+    public void Should_Have_Error_When_ConfirmPassword_Is_Empty()
+    {
+        // Arrange
+        var dto = new RegisterUserDto
+        {
+            UserName = "Test1",
+            Password = "TesT*00",
+            ConfirmPassword = "",
+            Email = "Test@mail.com",
+            FirstName = "Test",
+            LastName = "Test",
+            PhoneNumber = "955587878895"
+
+        };
+        // Act
+        var result = _validator.TestValidate(new RegisterUserRequest(dto));
+        // Assert
+        result.ShouldHaveValidationErrorFor(x => x.RegisterUserDto.ConfirmPassword)
+            .WithErrorMessage("ConfirmPassword is required");
+    }
+
+    [Test]
+    public void Should_Have_Error_When_ConfirmPassword_Is_Not_Valid()
+    {
+        // Arrange
+        var dto = new RegisterUserDto
+        {
+            UserName = "Test1",
+            Password = "TesT*00",
+            ConfirmPassword = "TesT",
+            Email = "Test@mail.com",
+            FirstName = "Test",
+            LastName = "Test",
+            PhoneNumber = "955587878895"
+
+        };
+        // Act
+        var result = _validator.TestValidate(new RegisterUserRequest(dto));
+        // Assert
+        result.ShouldHaveValidationErrorFor(x => x.RegisterUserDto.ConfirmPassword)
+            .WithErrorMessage("Passwords do not match");
+    }
+
+    [Test]
+    public void Should_Have_Error_When_Email_Is_Empty()
+    {
+        // Arrange
+        var dto = new RegisterUserDto
+        {
+            UserName = "Test1",
+            Password = "TesT*00",
+            ConfirmPassword = "TesT*00",
+            Email = "",
+            FirstName = "Test",
+            LastName = "Test",
+            PhoneNumber = "955587878895"
+
+        };
+        // Act
+        var result = _validator.TestValidate(new RegisterUserRequest(dto));
+        // Assert
+        result.ShouldHaveValidationErrorFor(x => x.RegisterUserDto.Email)
+            .WithErrorMessage("Email is required");
+    }
+
+    [Test]
+    public void Should_Have_Error_When_Email_Is_Not_Valid()
+    {
+        // Arrange
+        var dto = new RegisterUserDto
+        {
+            UserName = "Test1",
+            Password = "TesT*00",
+            ConfirmPassword = "TesT*00",
+            Email = "345QWE",
+            FirstName = "Test",
+            LastName = "Test",
+            PhoneNumber = "955587878895"
+
+        };
+        // Act
+        var result = _validator.TestValidate(new RegisterUserRequest(dto));
+        // Assert
+        result.ShouldHaveValidationErrorFor(x => x.RegisterUserDto.Email)
+            .WithErrorMessage("Invalid email address");
+    }
+
+    [Test]
+    public void Should_Have_Error_When_FirstName_Is_Empty()
+    {
+        // Arrange
+        var dto = new RegisterUserDto
+        {
+            UserName = "Test1",
+            Password = "TesT*00",
+            ConfirmPassword = "TesT*00",
+            Email = "Test@mail.com",
+            FirstName = "",
+            LastName = "Test",
+            PhoneNumber = "955587878895"
+        };
+        // Act
+        var result = _validator.TestValidate(new RegisterUserRequest(dto));
+        // Assert
+        result.ShouldHaveValidationErrorFor(x => x.RegisterUserDto.FirstName)
+            .WithErrorMessage("First name is required");
+    }
+
+    [Test]
+    public void Should_Have_Error_When_LastName_Is_Empty()
+    {
+        // Arrange
+        var dto = new RegisterUserDto
+        {
+            UserName = "Test1",
+            Password = "TesT*00",
+            ConfirmPassword = "TesT*00",
+            Email = "Test@mail.com",
+            FirstName = "Test",
+            LastName = "",
+            PhoneNumber = "955587878895"
+
+        };
+        // Act
+        var result = _validator.TestValidate(new RegisterUserRequest(dto));
+        // Assert
+        result.ShouldHaveValidationErrorFor(x => x.RegisterUserDto.LastName)
+            .WithErrorMessage("Last name is required");
+    }
+
+    [Test]
+    public void Should_Have_Error_When_PhoneNumber_Is_Empty()
+    {
+        // Arrange
+        var dto = new RegisterUserDto
+        {
+            UserName = "Test1",
+            Password = "TesT*00",
+            ConfirmPassword = "TesT*00",
+            Email = "Test@mail.com",
+            FirstName = "Test",
+            LastName = "Test",
+            PhoneNumber = ""
+
+        };
+        // Act
+        var result = _validator.TestValidate(new RegisterUserRequest(dto));
+        // Assert
+        result.ShouldHaveValidationErrorFor(x => x.RegisterUserDto.PhoneNumber)
+            .WithErrorMessage("Phone number is required");
+    }
+
+
+    [Test]
+    public void Should_Have_Error_When_PhoneNumber_Is_Not_Valid()
+    {
+        // Arrange
+        var dto = new RegisterUserDto
+        {
+            UserName = "Test1",
+            Password = "TesT*00",
+            ConfirmPassword = "TesT*00",
+            Email = "Test@mail.com",
+            FirstName = "Test",
+            LastName = "Test",
+            PhoneNumber = "Test78"
+
+        };
+        // Act
+        var result = _validator.TestValidate(new RegisterUserRequest(dto));
+        // Assert
+        result.ShouldHaveValidationErrorFor(x => x.RegisterUserDto.PhoneNumber)
+            .WithErrorMessage("Phone number must be valid and contain 7–12 digits");
+    }
+
+    [Test]
+    public void Should_Not_Have_Error_When_User_Is_Valid()
+    {
+        // Arrange
+        var dto = new RegisterUserDto
+        {
+            UserName = "Test1",
+            Password = "TesT*00",
+            ConfirmPassword = "TesT*00",
+            Email = "Test@mail.com",
+            FirstName = "Test",
+            LastName = "Test",
+            PhoneNumber = "955587878898"
+        };
+        // Act
+        var result = _validator.TestValidate(new RegisterUserRequest(dto));
+        // Assert
+        result.ShouldNotHaveAnyValidationErrors();
+    }
+
+}
