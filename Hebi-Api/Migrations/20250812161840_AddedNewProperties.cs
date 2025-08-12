0d078f2190588de5e2388bb61d381ff754b11516
Aidar Kadyraliev <aidar.kadyraliev@etnatrader.com>
2025-08-12 22:18:57 +0600

    Added a migration

---
+++ Hebi-Api/Migrations/20250812161840_AddedNewProperties.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hebi_Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedNewProperties : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Sex",
+                table: "AspNetUsers",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Sex",
+                table: "AspNetUsers");
+        }
+    }
+}
