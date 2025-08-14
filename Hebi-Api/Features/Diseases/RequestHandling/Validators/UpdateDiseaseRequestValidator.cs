b585546b0a43ad8dd589d04998a7a801962df1c3
Aidar Kadyraliev <aidar.kadyraliev@etnatrader.com>
2025-08-14 20:08:33 +0600

    Fix disease endpoints

---
+++ Hebi-Api/Features/Diseases/RequestHandling/Validators/UpdateDiseaseRequestValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using Hebi_Api.Features.Core.DataAccess.UOW;
+using Hebi_Api.Features.Diseases.RequestHandling.Requests;
+
+namespace Hebi_Api.Features.Diseases.RequestHandling.Validators;
+
+public class UpdateDiseaseRequestValidator : AbstractValidator<UpdateDiseaseRequest>
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    public UpdateDiseaseRequestValidator(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+        RuleFor(c => c.DiseaseId).MustAsync(async (diseaseId, cancellationToken)
+                                => await _unitOfWork.DiseaseRepository.ExistAsync(diseaseId)).
+                                WithMessage("The disease with this id doesn't exist");
+    }
+}
