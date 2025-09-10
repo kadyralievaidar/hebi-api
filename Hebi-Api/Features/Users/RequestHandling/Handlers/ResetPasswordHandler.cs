dfbe66c62fe1d506ebbe40111eb99a9d78a66a72
Aidar Kadyraliev <aidar.kadyraliev@etnatrader.com>
2025-09-10 20:31:05 +0600

    Added a password reset logic

---
+++ Hebi-Api/Features/Users/RequestHandling/Handlers/ResetPasswordHandler.cs
@@ -0,0 +1,35 @@
+﻿using Hebi_Api.Features.Core.Common.RequestHandling;
+using Hebi_Api.Features.Users.RequestHandling.Requests;
+using Hebi_Api.Features.Users.Services;
+using MediatR;
+
+namespace Hebi_Api.Features.Users.RequestHandling.Handlers;
+
+/// <summary>
+///     Reset password handler
+/// </summary>
+public class ResetPasswordHandler : IRequestHandler<ResetPasswordRequest, Response>
+{
+    private readonly ILogger<ResetPasswordHandler> _logger;
+    private readonly IUsersService _service;
+
+    public ResetPasswordHandler(ILogger<ResetPasswordHandler> logger, IUsersService service)
+    {
+        _logger = logger;
+        _service = service;
+    }
+
+    public async Task<Response> Handle(ResetPasswordRequest request, CancellationToken cancellationToken)
+    {
+        try
+        {
+            await _service.ResetPassword(request.Dto);
+            return Response.Ok(request.Id);
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e.Message);
+            return Response.BadRequest(request.Id, e);
+        }
+    }
+}
---
+++ Hebi-Api/Features/Users/RequestHandling/Requests/ChangePasswordRequest.cs
@@ -0,0 +1,20 @@
+﻿using Hebi_Api.Features.Core.Common.RequestHandling;
+using Hebi_Api.Features.Users.Dtos;
+
+namespace Hebi_Api.Features.Users.RequestHandling.Requests;
+
+/// <summary>
+///     Change password request
+/// </summary>
+public class ChangePasswordRequest : Request<Response>
+{
+    /// <summary>
+    ///     Change password dto
+    /// </summary>
+    public ChangePasswordDto Dto { get; set; }
+
+    public ChangePasswordRequest(ChangePasswordDto dto)
+    {
+        Dto = dto;
+    }
+}
