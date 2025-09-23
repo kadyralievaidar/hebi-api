e24182f42fd6f7b2662ebad58ed1fbc3d62f3353
Aidar Kadyraliev <aidar.kadyraliev@etnatrader.com>
2025-09-23 11:55:29 +0600

    Fix assign handler

---
+++ Hebi-Api/Features/ShiftTemplates/RequestHandling/Handlers/AssignShiftRequestHandler.cs
@@ -0,0 +1,32 @@
+﻿using Hebi_Api.Features.Core.Common.RequestHandling;
+using Hebi_Api.Features.Shifts.RequestHandling.Requests;
+using Hebi_Api.Features.Shifts.Services;
+using MediatR;
+
+namespace Hebi_Api.Features.ShiftTemplates.RequestHandling.Handlers;
+
+public class AssignShiftRequestHandler : IRequestHandler<AssignShiftRequest, Response>
+{
+    private readonly ILogger<AssignShiftRequestHandler> _logger;
+    private readonly IShiftsService _shiftService;
+
+    public AssignShiftRequestHandler(ILogger<AssignShiftRequestHandler> logger, IShiftsService shiftService)
+    {
+        _logger = logger;
+        _shiftService = shiftService;
+    }
+
+    public async Task<Response> Handle(AssignShiftRequest request, CancellationToken cancellationToken)
+    {
+        try
+        {
+            await _shiftService.AssignShift(request.DoctorId, request.ShiftId);
+            return Response.Ok(request.Id);
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, e.Message);
+            return Response.BadRequest(request.Id, e);
+        }
+    }
+}
